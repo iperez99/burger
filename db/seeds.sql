68e06d4a408bbf53d9680f1a1ae1fcab220aef73
Qbisst <ismaelaperez@outlook.com>
2019-05-14 20:33:43 -0400

    schema and seeds done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('American Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Big Skinny', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', TRUE);
