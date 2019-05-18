e2393b219e1793bd38fdf0e3d20b170e1f1237b6
Qbisst <ismaelaperez@outlook.com>
2019-05-17 23:01:51 -0400

    using cats app for orm

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,8 @@
 // Import MySQL connection.
 var connection = require("./connection.js");
 
-var orm = {
-    all: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    update: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
 
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    },
-
-}
 
 
 module.exports = orm;
