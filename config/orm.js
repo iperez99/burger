d7a54e5cb75b7c2a1a0a86867df6642bab5301bb
Qbisst <ismaelaperez@outlook.com>
2019-05-17 23:10:52 -0400

    fixed orm js file

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,56 @@
 // Import MySQL connection.
 var connection = require("./connection.js");
 
+var orm = {
+  select: function (table, cb) {
+    var query = `SELECT * FROM ??`;
 
+    connection.query(query, [table], function (err, results) {
+      if (err) {
+        throw err;
+      }
+      console.log(`showing all the burgers`)
+      console.log(results);
+      cb(results);
+    });
+  },
+
+  create: function (table, column, value) {
+    var insertQuery = `INSERT INTO ?? (??) VALUES(?)`
+
+    connection.query(insertQuery, [table, column, value], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(`adding burger to db...`)
+      console.log(result);
+    });
+  },
+
+  update: function (table, column, newVal, whereCol, whereVal) {
+    var updateQuery = `UPDATE ?? SET ?? = ? WHERE ?? = ?`
+
+    connection.query(updateQuery, [table, column, newVal, whereCol, whereVal], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(`updating db...`)
+      console.log(result);
+    });
+  },
+
+  delete: (table, column, value) => {
+    var deleteQuery = `DELETE FROM ?? WHERE ?? = ?`
+
+    connection.query(deleteQuery, [table, column, value], function (err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log(`deleting burger..`)
+      console.log(result);
+    });
+  }
+}
 
 
 
