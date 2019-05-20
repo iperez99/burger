89ae74613ed8ecf7445873429f20e8443448bb73
Qbisst <ismaelaperez@outlook.com>
2019-05-19 23:17:40 -0400

    updated orm.js file

--- config/orm.js
+++ config/orm.js
@@ -34,66 +34,51 @@ function objToSql(ob) {
 
 var orm = {
   // Display all burgers in the db.
-  all: function (table, cb) {
-      var queryString = "SELECT * FROM " + table + ";";
+  selectAll: function (table, cb) {
+    var queryString = "SELECT * FROM " + table + ";";
 
-      connection.query(queryString, function (err, result) {
-          if (err) {
-              throw err;
-          }
-          cb(result);
-      });
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
   },
   // Add a burger to the db.
-  create: function (table, cols, vals, cb) {
-      var queryString = "INSERT INTO " + table;
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += printQuestionMarks(vals.length);
-      queryString += ") ";
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-      console.log(queryString);
+    console.log(queryString);
 
-      connection.query(queryString, vals, function (err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
-      });
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
   },
   // Set burger devoured status to true.
-  update: function (table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-      console.log(queryString);
+    console.log(queryString);
 
-      connection.query(queryString, function (err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
-      });
-  },
-  // Delete a burger from the db.
-  delete: function (table, condition, cb) {
-      var queryString = "DELETE FROM " + table;
-      queryString += " WHERE ";
-      queryString += condition;
-
-      console.log(queryString);
-
-      connection.query(queryString, function (err, result) {
-          if (err) {
-              throw err
-          }
-          cb(result);
-      });
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
   }
 };
 
