0977fe22bd3832489c062aad2e7e5b8ae7d8b490
Qbisst <ismaelaperez@outlook.com>
2019-05-20 00:08:38 -0400

    removed optional Bootstrap scripts and added a working Jquery

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,11 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name VARCHAR(255) NOT NULL,
+	id int NOT NULL
+	AUTO_INCREMENT,
+	burger_name VARCHAR
+	(255) NOT NULL,
 	devoured BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+	(id)
 );
